4ac7ab46af5ed51ad4e1bc93e7f2e6133dab56e2
Sofía Suazo <sls936@nyu.edu>
2019-04-15 01:36:49 -0400

    redirecting

--- WEEK2/partC/app4url/index.js
+++ WEEK2/partC/app4url/index.js
@@ -7,21 +7,14 @@ const path = require('path')
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 
-var insults_dict = {
-  0: "bad, stupid and amateur",
-  1: "awful, predictible and arrogant",
-  2: "boring, careless and aggresive",
-  3: "fussy, ugly and noisey",
-  4: "plain, unoriginal and overrated"
-};
-
-
 app.get('/', function(req, res){
-    var myText = req.query.mytext; //mytext is the name of your input box
-    if (myText === undefined){
+    var url = req.query.url;
+    if (url === undefined){
       res.sendFile(path.join(__dirname + '/index.html'))
     }else{
-      res.send('the band ' + myText + ' is so ' + insults_dict[Math.floor(Math.random(insults_dict)*4)]);
+      res.type('html');
+      // res.render(myText);
+      res.redirect(url);
     }
 });
 
